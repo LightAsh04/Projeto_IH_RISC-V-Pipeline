234a1a4ab2efbfd96a33b35b696f98a7c38e3491
arthurmarsar0 <108816696+arthurmarsar0@users.noreply.github.com>
2023-08-19 21:02:22 -0300

    Adição de LH e LB

--- design/datamemory.sv
+++ design/datamemory.sv
@@ -38,6 +38,10 @@ module datamemory #(
       case (Funct3)
         3'b010:  //LW
         rd <= Dataout;
+        3'b001:  //LB
+            rd <= {{16{1'b0}}, Dataout[7:0]};
+        3'b000:  //LH
+            rd <= {{24{1'b0}}, Dataout[7:0]};
         default: rd <= Dataout;
       endcase
     end else if (MemWrite) begin
