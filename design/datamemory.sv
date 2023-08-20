bc33d581e68dd5154f81e004a1bd0c4d88cc6bfa
arthurmarsar0 <108816696+arthurmarsar0@users.noreply.github.com>
2023-08-19 21:29:15 -0300

    Alterando lh
    Tamanho de Dataout estava errado

--- design/datamemory.sv
+++ design/datamemory.sv
@@ -39,7 +39,7 @@ module datamemory #(
         3'b010:  //LW
         rd <= Dataout;
         3'b001:  //LB
-            rd <= {{16{1'b0}}, Dataout[7:0]};
+            rd <= {{16{1'b0}}, Dataout[15:0]};
         3'b000:  //LH
             rd <= {{24{1'b0}}, Dataout[7:0]};
         default: rd <= Dataout;
