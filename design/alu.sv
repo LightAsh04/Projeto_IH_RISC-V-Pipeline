0767cdfdfaba00135c38916994fefbdc8a7b16ab
xupenio04 <106712324+xupenio04@users.noreply.github.com>
2023-08-22 19:27:48 -0300

    Inclusão do LUI na alu

--- design/alu.sv
+++ design/alu.sv
@@ -41,6 +41,8 @@ module alu#(
                     ALUResult = (SrcA >= SrcB) ? 1 : 0;
             4'b1100: // xor 12
                    ALUResult = SrcA ^ SrcB;
+           4'b1101: // LUI 13
+                   ALUResult = SrcB;
             default:
                     ALUResult = 0;
             endcase
