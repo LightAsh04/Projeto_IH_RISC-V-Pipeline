79152f2146d11befa77d320d763f9ceeb91708ad
aline-fortaleza <108816622+aline-fortaleza@users.noreply.github.com>
2023-08-22 16:42:32 -0300

    FINALMENTE O SLT ARRUMADO!!!!!!!!!!

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -46,7 +46,7 @@ module ALUController (
       ((ALUOp == 2'b01) && (Funct3==3'b001)) ||  // BNE
       ((ALUOp == 2'b01) && (Funct3==3'b100)) ||  // BLT
       ((ALUOp == 2'b01) && (Funct3==3'b101)) ||  // BGE
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010) && (Funct7 != 7'b0100000)) ||
+      //((ALUOp == 2'b10) && (Funct3 == 3'b010) && (Funct7 != 7'b0100000)) ||
       ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000)); // XOR  // R\I-<
        
 
