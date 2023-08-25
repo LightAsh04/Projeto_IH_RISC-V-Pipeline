981766cebbd6153c9ca1c2e1f44e67737c6fd48b
xupenio04 <106712324+xupenio04@users.noreply.github.com>
2023-08-25 11:47:02 -0300

    Colocando Halt no risc-v

--- design/RISC_V.sv
+++ design/RISC_V.sv
@@ -19,7 +19,7 @@ module riscv #(
 );
 
   logic [6:0] opcode;
-  logic ALUSrc, JaltoReg, MemtoReg, RegWrite, MemRead, MemWrite, Branch, JalrSel;
+  logic Halt,ALUSrc, JaltoReg, MemtoReg, RegWrite, MemRead, MemWrite, Branch, JalrSel;
   logic [1:0] ALUop;
   logic [1:0] ALUop_Reg;
   logic [6:0] Funct7;
@@ -28,6 +28,7 @@ module riscv #(
 
   Controller c (
       opcode,
+      Halt,
       ALUSrc,
       JaltoReg,
       MemtoReg,
@@ -56,6 +57,7 @@ module riscv #(
       MemWrite,
       MemRead,
       Branch,
+      Halt,
       JalrSel,
       ALUop,
       Operation,
